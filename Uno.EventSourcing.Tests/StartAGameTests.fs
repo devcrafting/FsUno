abc71d492169c7a75911af98125328110086ed24
Clément Bouillier <clement.bouillier@devcrafting.com>
2014-10-27 11:36:16 +0100

    Start a game - nominal case

---
+++ Uno.EventSourcing.Tests/StartAGameTests.fs
@@ -0,0 +1,24 @@
+﻿module Uno.EventSourcing.Tests.``Start a game``
+
+open NUnit.Framework
+open FsUnit
+
+type StartAGame =
+    { NbPlayers: int }
+
+type GameStarted =
+    { GameId: int }
+
+type State =
+    { Started: bool }
+
+let InitialState = { Started = false }
+
+let startAGame (command : StartAGame) state =
+    [ { GameId = 1} ]
+
+[<Test>]
+let ``with more than 2 players`` () =
+    let events = startAGame { NbPlayers = 3 } InitialState 
+    events |> should contain { GameId = 1 }
+    events |> should haveLength 1
