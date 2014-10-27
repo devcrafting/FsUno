69bf990d020a8ea29eb3980f3b98fc2f8bf3b533
Clément Bouillier <clement.bouillier@devcrafting.com>
2014-10-27 12:24:01 +0100

    StartAGame with less than 3 players

--- Uno.EventSourcing.Tests/StartAGameTests.fs
+++ Uno.EventSourcing.Tests/StartAGameTests.fs
@@ -6,6 +6,11 @@ open FsUnit
 open Game
 
 [<Test>]
-let ``with more than 2 players`` () =
+let ``with 3 players`` () =
     startAGame { NbPlayers = 3 } InitialState 
         |> should equal [ { GameId = 1 } ]
+
+[<Test>]
+let ``with less than 3 players`` () =
+    (fun () -> startAGame { NbPlayers = 2 } InitialState |> ignore)
+        |> should throw typeof<System.Exception>
--- Uno.EventSourcing/Game.fs
+++ Uno.EventSourcing/Game.fs
@@ -12,4 +12,6 @@ type State =
 let InitialState = { Started = false }
 
 let startAGame (command : StartAGame) state =
+    if command.NbPlayers < 3 then
+        failwith "There should be at least 3 players to be fun."
     [ { GameId = 1} ]
