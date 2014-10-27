416f56ac9d23646adf2df0cc1a9276a9af37ffc1
Clément Bouillier <clement.bouillier@devcrafting.com>
2014-10-27 12:50:46 +0100

    Start a game already started

--- Uno.EventSourcing.Tests/StartAGameTests.fs
+++ Uno.EventSourcing.Tests/StartAGameTests.fs
@@ -5,12 +5,20 @@ open FsUnit
 
 open Game
 
+let StartAValidGame = { StartAGame.GameId = 1; NbPlayers = 3 }
+let AlreadyStartedGame = { Started = true }
+
 [<Test>]
 let ``with 3 players`` () =
-    startAGame { GameId = 1; NbPlayers = 3 } InitialState 
+    startAGame StartAValidGame InitialState 
         |> should equal [ { GameId = 1; NbPlayers = 3 } ]
 
 [<Test>]
 let ``with less than 3 players`` () =
     (fun () -> startAGame { GameId = 1; NbPlayers = 2 } InitialState |> ignore)
+        |> should throw typeof<System.Exception>
+
+[<Test>]
+let ``when already started`` () =
+    (fun () -> startAGame StartAValidGame AlreadyStartedGame |> ignore)
         |> should throw typeof<System.Exception>
--- Uno.EventSourcing/Game.fs
+++ Uno.EventSourcing/Game.fs
@@ -14,6 +14,8 @@ type State =
 let InitialState = { Started = false }
 
 let startAGame (command : StartAGame) state =
+    if state.Started then
+        failwith "Game is already started, cannot start again."
     if command.NbPlayers < 3 then
         failwith "There should be at least 3 players to be fun."
     [ { GameStarted.GameId = 1; NbPlayers = command.NbPlayers } ]
